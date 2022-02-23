2dc86e744e272e9d2137196a4a698610968633ae
Cristian Ramos <ramoscristian2704@gmail.com>
2022-02-23 18:19:43 -0300

    add profile, skills, edu-exp, projects, courses, aboutme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { EduExpComponent } from './edu-exp/edu-exp.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CoursesComponent } from './courses/courses.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProfileComponent,
+    AboutmeComponent,
+    EduExpComponent,
+    SkillsComponent,
+    CoursesComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/edu-exp/edu-exp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edu-exp',
+  templateUrl: './edu-exp.component.html',
+  styleUrls: ['./edu-exp.component.css']
+})
+export class EduExpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

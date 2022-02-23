c86641fda1e358286d1441f8c47b9d78431f6406
Cristian Ramos <ramoscristian2704@gmail.com>
2022-02-23 18:40:38 -0300

    Movido todo a componentes, add services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ProfileComponent } from './profile/profile.component';
-import { AboutmeComponent } from './aboutme/aboutme.component';
-import { EduExpComponent } from './edu-exp/edu-exp.component';
-import { SkillsComponent } from './skills/skills.component';
-import { CoursesComponent } from './courses/courses.component';
-import { ProjectsComponent } from './projects/projects.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { EduExpComponent } from './components/edu-exp/edu-exp.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
